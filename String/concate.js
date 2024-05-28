3e11049d96a40fea0dceaec929cbf6f2e0ebfd18
Pooja Singh <poojasingh31280@gmail.com>
2024-05-28 18:17:56 +0530

    string

---
+++ String/concate.js
@@ -0,0 +1,10 @@
+// String concat
+let text1 = "Hello";
+let text2 = "World";
+let text3 = text1.concat(" ", text2);
+console.log(text3)
+
+let text4 = "      Hello World!      ";
+let text5 = text4.trim();
+console.log(text5)
+

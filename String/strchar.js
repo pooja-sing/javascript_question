49c53dba81d044a63167857765fed499b7b07623
Pooja Singh <poojasingh31280@gmail.com>
2024-05-28 18:00:29 +0530

    string

---
+++ String/strchar.js
@@ -0,0 +1,13 @@
+// The at(position) Method
+// The charAt(position) Method
+// The charCodeAt(position) Method
+// Using property access [] like in arrays
+
+// var text = "HELLO WORLD";
+// const newtext=text.charAt(1)
+// console.log(newtext)
+
+
+let text = "HELLO WORLD";
+let char = text.charCodeAt(0);
+console.log(char)
